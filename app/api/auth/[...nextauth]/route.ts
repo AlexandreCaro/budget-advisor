531d07d76207ce595d83e0656a87cef1e7a68e03
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-12-01 10:45:19 +0300

    new commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
-import NextAuth from "next-auth"
+import NextAuth, { Session, User } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/lib/prisma"
+import { AdapterUser } from "@auth/core/adapters"
 
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -12,12 +13,18 @@ const handler = NextAuth({
     }),
   ],
   callbacks: {
-    session: async ({ session, user }) => {
+    session: async ({ session, user }: { session: Session; user: AdapterUser }) => {
       if (session?.user) {
         session.user.id = user.id;
       }
       return session;
     },
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    }
   },
 })
 
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,9 +1,26 @@
-import type { DefaultSession } from 'next-auth'
+import 'next-auth'
+import { User } from '@prisma/client'
 
 declare module 'next-auth' {
   interface Session {
     user: {
       id: string
-    } & DefaultSession['user']
+      name?: string | null
+      email?: string | null
+      image?: string | null
+    }
+  }
+
+  interface User {
+    id: string
+    name?: string | null
+    email?: string | null
+    image?: string | null
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id: string
   }
 } 
